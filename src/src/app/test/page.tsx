70bfd02598993f0f2cbba4f5d0defe56a37933b4
mitate-gengaku <mitategengaku@gmail.com>
2025-03-28 22:31:20 +0900

    fix

---
+++ src/src/app/test/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import "@/styles/test.css"
+import { useRef } from "react"
+
+const Test = () => {
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  const onClick = () => {
+    ref.current?.classList.add("flip")
+  }
+
+  return (
+    <div 
+      className="page"
+      ref={ref} 
+      onClick={() => onClick()}
+      >
+      <div className="w-[50px] h-[500px] absolute !right-0" />
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Test;
